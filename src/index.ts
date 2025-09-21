6f0f0fa231c2d5d24ab93eb534e39a08a569eb72
Subhransu Jena <subhransujena2468@gmail.com>
2025-09-21 08:12:55 +0530

    Initialize the src

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express ,{Express,Request,Response}from "express"
+import { PORT } from "./secrets";
+
+const app:Express = express();
+
+app.get("/",(req:Request,res:Response) => {
+    res.send("Hello World...")
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`app is listening...`)
+})
